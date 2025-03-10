71b4d619666ac1f775df767ca6a285578e7ff36c
Tushar <111505346+Tushar6396@users.noreply.github.com>
2025-03-10 11:14:17 +0530

    basic changes initialized

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
+dotenv.config();
 const app = express();
 const port = process.env.port || 4000;
+connectDB();
+connectCloudinary();
 
+//middlewares
 app.use(express.json());
 app.use(cors());
+
+//API endpoints
+app.use('/api/admin', adminRouter);
+// localhost:4000/api/admin
 
 app.get('/', (req, res) => {
   res.send('Hello from server');
